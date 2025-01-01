dcc5ff0df53aca6e9b4ceb6717f333712589d030
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-01-01 19:15:01 +0700

    Update

--- src/nr/main.py
+++ src/nr/main.py
@@ -1,12 +1,12 @@
 import os
 
-# from compress import compressVDO
+from compress import compressVDO
 from delete import deleteSourceVDO
 
 #######################################################
 ENCODE_QUALITY = 80
-sourceFolder = os.path.join("C:/", "Users", "nnnpo", "Desktop", "New folder")
+sourceFolder = os.path.join("E:/", "temp", "Videos")
 print(sourceFolder)
 
-# compressVDO(sourceFolder=sourceFolder, encodeQuality=ENCODE_QUALITY, dryrun=False)
+compressVDO(sourceFolder=sourceFolder, encodeQuality=ENCODE_QUALITY, dryrun=False)
 deleteSourceVDO(sourceFolder=sourceFolder, encodeQuality=ENCODE_QUALITY, dryrun=False)
